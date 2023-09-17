550d57af69941d9276e14ca89d5b3c9e9f0a0a24
Jhilam-Moumit <jhilammoumit@gmail.com>
2023-09-17 13:57:53 +0600

    final commmit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    int sum;
+    public static void main(String[] args) throws Exception {
+        App app=new App();
+        app.sum=365;
+        System.out.println("Hello, World!");
+        System.out.println(app.sum);
+        
+
+    }
+}
---
+++ src/joy.java
@@ -0,0 +1,5 @@
+public class joy{
+ public static void main(String[] args)  {
+        System.out.println("hi this is joy ");
+    }
+}
